e28fed8626086717d6c4a66ce5ae6e04351f9bf5
Adiro777 <112520123+Adiro777@users.noreply.github.com>
2026-01-04 17:17:58 -0800

    fixing base

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import {
 import ScrollToTop from './components/ScrollToTop.jsx';
 import Footer from './components/footer.jsx';
 
-import Navbar from '../components/navbar.jsx';
+import Navbar from './components/navbar.jsx';
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 import Projects from './pages/Projects.jsx';
